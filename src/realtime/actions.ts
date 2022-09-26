58a73a1ea92c3df4f5f4f702285095d86fb296e0
Krishna Mahato <krishhtrishh9304@gmail.com>
2022-09-27 02:10:02 +0530

    ADD: realtime data about online users

---
+++ src/realtime/actions.ts
@@ -0,0 +1,6 @@
+export const SocketActions = {
+  connection: "connection",
+  disconnect: "disconnect",
+  ADD_USER: "ADD_USER",
+  IS_USER_ONLINE: "IS_USER_ONLINE",
+};
--- src/realtime/index.ts
+++ src/realtime/index.ts
@@ -1,17 +1,46 @@
-import Socket, { Server } from "socket.io";
+import colors from "ansi-colors";
+import { Server, Socket } from "socket.io";
 import { server } from "..";
+import { SocketActions } from "./actions";
 
 // init socket server
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"],
-  },
-});
+const io = new Server(server);
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+let onlineUsers = new Map();
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+io.on(SocketActions.connection, (socket: Socket) => {
+  showAllOnlineUsers();
+
+  // populating online users
+  socket.on(SocketActions.ADD_USER, (userId) => {
+    onlineUsers.set(socket.id, userId);
+    showAllOnlineUsers();
+  });
+
+  socket.on(SocketActions.IS_USER_ONLINE, (userId: String) => {
+    let users: Array<String> = [...onlineUsers.values()];
+    socket.emit("ONLINE_STATUS", users.includes(userId));
+  });
+
+  socket.on(SocketActions.disconnect, () => {
+    console.log("Client disconnected: " + colors.red(socket.id));
+    onlineUsers.delete(socket.id);
   });
 });
+
+// show all online users
+const showAllOnlineUsers = () => {
+  let users: Array<String> = [];
+  onlineUsers.forEach((value) => {
+    users.push(value);
+  });
+  if (users.length > 0) {
+    console.log(colors.bold(colors.whiteBright("\n  Clients connected")));
+  } else {
+    console.log(colors.bold(colors.whiteBright("\n No  Clients connected")));
+  }
+
+  console.log(
+    colors.cyanBright(colors.bold(`\n  Total connections: ${users.length} `))
+  );
+};
